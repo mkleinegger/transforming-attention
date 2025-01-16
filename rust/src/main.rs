7ec656dda66a26f28f8736827336ffaca187dbfd
Lukas <lukas.mah@outlook.at>
2025-01-17 00:36:14 +0100

    feat: add bert embeddings for candle

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,13 +1,23 @@
-use candle_core::{Device, Tensor};
+use tokenizers::tokenizer::{Result, Tokenizer};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    let tokenizer = Tokenizer::from_pretrained("bert-base-cased", None)?;
+    let encoding = tokenizer.encode(("Hello Tokenizer!", "Testing out if this works."), true)?;
 
-    let device = Device::Cpu;
-    let a = Tensor::randn(0f32, 1., (2, 3), &device)?;
-    let b = Tensor::randn(0f32, 1., (3, 4), &device)?;
+    println!("tokens: {:?}", encoding.get_tokens());
+    println!("ids: {:?}", encoding.get_ids());
 
-    let c = a.matmul(&b)?;
-    println!("{c}");
     Ok(())
 }
+
+// fn test_candle() -> Result<(), Box<dyn std::error::Error>> {
+//     println!("Hello, world!");
+//
+//     let device = Device::Cpu;
+//     let a = Tensor::randn(0f32, 1., (2, 3), &device)?;
+//     let b = Tensor::randn(0f32, 1., (3, 4), &device)?;
+//
+//     let c = a.matmul(&b)?;
+//     println!("{c}");
+//     Ok(())
+// }
